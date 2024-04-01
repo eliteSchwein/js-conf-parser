a2bc4f0b058e733c026dd09ed666f7807d2d0142
Thomas Ludwig <business@eliteschw31n.de>
2024-04-01 22:02:59 +0200

    feat: use tsc instead of esbuild

---
+++ dist/index.d.ts
@@ -0,0 +1,11 @@
+/**
+ * This module implements a conf file parser.
+ * @packageDocumentation
+ * @module ja-conf-parser
+ */
+/**
+ * @public
+ * @param path Path to the config file
+ * @param filename Name of the config file
+ */
+export default function parseConfig(path: string, filename: string): {};
